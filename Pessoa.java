4ccc53b8bcfee21b7790c0327a4622e49da71331
Andrey_Del_Bello <andrey.delbello2002@gmail.com>
2022-02-22 17:34:48 +0000

    adicionando arquivo

---
+++ Pessoa.java
@@ -0,0 +1,26 @@
+public class Pessoa{
+
+    private String nome;
+    private String endereco;
+    private String telefone;
+
+    public String getNome() {
+        return nome;
+    }
+    public void setNome(String nome) {
+        nome = this.nome;
+    }
+    public String getEndereco() {
+        return endereco;
+    }
+    public void setEndereco(String endereco) {
+        endereco = this.endereco;
+    }
+    public String getTelefone() {
+        return telefone;
+    }
+    public void setTelefone(String telefone) {
+        telefone = this.telefone;
+    }
+    
+}
