227c32422e96257541b6f292a6428f1554ef09ed
Andrey_Del_Bello <andrey.delbello2002@gmail.com>
2022-02-22 18:43:46 +0000

    Adicionando Arquivo

---
+++ Empregado.java
@@ -0,0 +1,11 @@
+public class Empregado extends Pessoa{
+    private double salario;
+    
+    public double setSalario(){
+        return this.salario;
+    }
+
+    public void setSalario(double salario){
+        salario = this.salario;
+    }
+}
--- Principal.java
+++ Principal.java
@@ -1,6 +1,27 @@
 public class Principal {
     public static void main(String[] args) {
         
+        Empregado empregado = new Empregado();
+        empregado.setNome("AnaBelle");
+        empregado.setEndereco("Pompeia");
+        empregado.setTelefone("ZZZ");
+        empregado.setSalario(1800);
+
+        Empregado empregado2=new Empregado();
+        empregado2.setNome("Andrey");
+        empregado2.setEndereco("Pompeia");
+        empregado2.setTelefone("14997884295");
+        empregado2.setSalario(1500);
+
+        System.out.println("Nome: " + empregado.getNome());
+        System.out.println("Endereco: " + empregado.getEndereco());
+        System.out.println("Telefone: " + empregado.getTelefone());
+        System.out.println("Salario: " + empregado.getSalario());
+
+        System.out.println("Nome: " + empregado2.getNome());
+        System.out.println("Endereco: " + empregado2.getEndereco());
+        System.out.println("Telefone: " + empregado2.getTelefone());
+        System.out.println("Salario: " + empregado2.getSalario());
     }
     
 }
